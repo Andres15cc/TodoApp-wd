e7816a7cb8d0627606dfa7a499db6b38f5140d06
delgadillo <delgadillow79@gmail.com>
2026-02-18 10:25:43 -0400

    subiendo actualizacion de login

--- app.js
+++ app.js
@@ -1,7 +1,28 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const path = require('path');
+const mongoose = require('mongoose');
 
+
+
+const loginRouter = require('./controllers/login');
+
+
+// Rutas
+app.use('/api/login', loginRouter);
+
+module.exports = app;
+
+(async()=> { 
+
+    try {
+        await mongoose.connect(process.env.MONGO_URI_TEST);
+        console.log('Conectado a MongoDB');
+    } catch (error) {
+        console.log(error);
+    }
+})();
 
 
 //Rutas Frontend
@@ -11,6 +32,15 @@ app.use('/signup', express.static(path.resolve('views', 'signup')));
 app.use('/login', express.static(path.resolve('views', 'login')));
 app.use('/components', express.static(path.resolve('views', 'components')));
 app.use('/images', express.static(path.resolve('img')));
+
+
+//Rutas backend
+
+
+app.use('/api/login', loginRouter);
+
+
+
 
 
 module.exports = app;
